694b89d5c2cce2ee94ab33be634a46dcbfca5051
Ashok K <kambalaashok4@gmail.com>
2026-02-27 23:22:55 +0530

    aupdated aws ecr

--- app/views.py
+++ app/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 def linkedin_view(request):
     context = {
-        "linkedin_url": "https://www.linkedin.com/in/"
+        "linkedin_url": "https://www.linkedin.com/in/ashok-kambala-3a9157220/"
     }
     return render(request, "profile/linkedin.html", context)
 
